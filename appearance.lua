d25fe918a356501ee4cb1e100a248854685c35c2
yihao <yihao039@gmail.com>
2026-03-07 15:08:53 +0800

    use iscript

--- appearance.lua
+++ appearance.lua
@@ -5,13 +5,13 @@ local M = {}
 function M.apply(config)
 	-- Font
 	config.font = wezterm.font_with_fallback({
-		"Monaspace Xenon",
 		"Fira Code iScript",
-		"Fira Code",
+		"Monaspace Xenon",
+		"Ubuntu Mono",
 	})
-	config.font_size = 13
-	config.line_height = 1.2
-	config.cell_width = 0.9
+	config.font_size = 12
+	config.line_height = 1
+	config.cell_width = 1
 
 	-- Cursor
 	config.default_cursor_style = "BlinkingBar"
@@ -20,7 +20,7 @@ function M.apply(config)
 	config.force_reverse_video_cursor = false
 
 	-- Tab bar
-	config.enable_tab_bar = true
+	config.enable_tab_bar = false
 	-- config.tab_bar_at_bottom = true
 	-- config.hide_tab_bar_if_only_one_tab = true
 	config.use_fancy_tab_bar = true
@@ -28,24 +28,26 @@ function M.apply(config)
 
 	-- Window
 	config.window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
+		left = 4,
+		right = 4,
+		top = 4,
+		bottom = 4,
 	}
 	config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-	config.window_background_opacity = 0
 
 	-- Colors & rendering
 	config.color_scheme = "rose-pine-moon"
 	-- config.color_scheme = "Catppuccin Mocha"
-	config.front_end = "OpenGL"
-	config.win32_system_backdrop = "Mica"
+	config.front_end = "WebGpu"
+	-- config.win32_system_backdrop = "Mica"
+	-- config.window_background_opacity = 0
+	config.win32_system_backdrop = "Acrylic"
+	config.window_background_opacity = 0.5
 
 	-- Misc
 	config.use_ime = true
-	config.term = "xterm-256color"
-	config.automatically_reload_config = true
+	config.term = "wezterm"
+	config.automatically_reload_config = false
 end
 
 return M
